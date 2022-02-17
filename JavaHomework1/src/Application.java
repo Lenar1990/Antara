162a13fe84007909bcc89857957196432f63b0dc
Lenar1990 <timerlenar@gmail.com>
2022-02-17 18:18:25 +0300

    Добавил генератор методов git commit ^O ^X
    O
    X

--- JavaHomework1/src/Application.java
+++ JavaHomework1/src/Application.java
@@ -7,14 +7,15 @@ public class Application {
 
         Kotik Vasya = new Kotik();
         int count1 = Kotik.count;
-        Vasya.setMeow("мяу");
+        /*Vasya.setMeow("мяу");
         Vasya.setWeight(3000);
         Vasya.setPrettiness(5.5);
         Vasya.setName("Вася");
-        Vasya.setFood("китекат");
+        Vasya.setFood("китекат");*/
 
         Kotik Boris = new Kotik();
-        int count2 = Kotik.count;Boris.setMeow("Мяу");
+        int count2 = Kotik.count;
+        /*Boris.setMeow("Мяу");
         Boris.setWeight(4000);
         Boris.setPrettiness(7.5);
         Boris.setName("Борис");
@@ -27,7 +28,12 @@ public class Application {
         else {
             System.out.println("они мурлычат по-разному");
         }
-        System.out.println("создано " + count2 + " котика");
+        System.out.println("создано " + count2 + " котика");*/
+
+        Vasya.liveAnotherDay();
+        Vasya.liveAnotherDay();
+
+        //Boris.liveAnotherDay();
 
     }
 }
--- JavaHomework1/src/model/Kotik.java
+++ JavaHomework1/src/model/Kotik.java
@@ -1,4 +1,5 @@
 package model;
+import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.random.*;
 
@@ -14,6 +15,8 @@ public class Kotik {
     private String meow;
     private String food;
     private double prettiness;
+    //private int method1;
+
 
     public static int count;
 
@@ -61,31 +64,87 @@ public class Kotik {
 
     }
 
-    public liveAnotherDay() {
 
+
+    public boolean liveAnotherDay() {
+        for (int i = 1; i <= 26; i++) {
+        double method = 1 + 6 * Math.random();
+        int method1 = (int) Math.round(method);
+
+            switch (method1) {
+                case 1:
+                    jump();
+                    System.out.println("я прыгаю");
+                    break;
+                case 2:
+                    lick_itself();
+                    System.out.println("я облизываюсь");
+                    break;
+                case 3:
+                    sharpen_claws();
+                    System.out.println("я точу когти");
+                    break;
+                case 4:
+                    climb();
+                    System.out.println("я лазаю");
+                    break;
+                case 5:
+                    sleep();
+                    System.out.println("я сплю");
+                    break;
+                case 6:
+                    eat();
+                    System.out.println("я ем");
+                    break;
+            }
+
+
+        }
+        return true;
     }
 
+
     private boolean jump()    {
+        satiety--;
+        if (satiety<=0) {
+            System.out.println("я хочу есть");
+        }
         return true;
     }
 
 
     private boolean lick_itself()    {
+        satiety--;
+        if (satiety<=0) {
+            System.out.println("я хочу есть");
+        }
         return true;
     }
 
 
     private boolean sharpen_claws()      {
+        satiety--;
+        if (satiety<=0) {
+            System.out.println("я хочу есть");
+        }
         return true;
     }
 
 
     private boolean sleep()    {
+        satiety--;
+        if (satiety<=0) {
+            System.out.println("я хочу есть");
+        }
         return true;
     }
 
 
     private boolean climb()    {
+        satiety--;
+        if (satiety<=0) {
+            System.out.println("я хочу есть");
+        }
         return true;
     }
 
@@ -93,7 +152,7 @@ public class Kotik {
     private void eat()    {
         satiety++;
         if (satiety==100) {
-            System.out.println("I am full");
+            System.out.println("я наелся");
         }
     }
 
